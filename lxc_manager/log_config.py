25f043d026d2d114feb71f2bd12fdadeb7b52868
Mathieu <mathieu.collet@gmail.com>
2023-04-12 15:21:53 +0200

    Add colorlog

--- lxc_manager/log_config.py
+++ lxc_manager/log_config.py
@@ -1,7 +1,31 @@
 import logging
 import sys
+import colorlog
 
 
 def setup_logging(log_level: str):
-    log_format = "%(asctime)s [%(levelname)s] %(name)s: %(message)s"
-    logging.basicConfig(level=log_level, format=log_format, stream=sys.stdout)
+    log_level = getattr(logging, log_level.upper(), "INFO")
+
+    log_format = "%(log_color)s%(asctime)s [%(levelname)s] %(name)s: %(message)s"
+
+    # Configuration de colorlog
+    colored_formatter = colorlog.ColoredFormatter(
+        log_format,
+        datefmt="%Y-%m-%d %H:%M:%S",
+        log_colors={
+            "DEBUG": "cyan",
+            "INFO": "green",
+            "WARNING": "yellow",
+            "ERROR": "red",
+            "CRITICAL": "red,bg_white",
+        },
+    )
+
+    # Configuration du gestionnaire de log
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setFormatter(colored_formatter)
+
+    # Configuration du logger
+    logger = logging.getLogger()
+    logger.setLevel(log_level)
+    logger.addHandler(handler)
